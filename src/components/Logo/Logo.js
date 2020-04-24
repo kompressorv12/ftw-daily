e03073fce3d297e2350b2dd66d8fb67e311a6ca6
Ben Berry <ben.berry@thermofisher.com>
2020-04-24 14:19:51 -0700

    fixes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 import config from '../../config';
 import IconLogo from './IconLogo';
-import LogoImage from './Eyeballs-logo.png';
+import LogoImage from './saunatime-logo.png';
 import css from './Logo.css';
 
 const Logo = props => {
